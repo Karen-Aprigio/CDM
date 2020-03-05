50e0f6741a255abd5d1b3ae8d7f9b675b6568bf0
Rayane1007 <rayane.gomes.1007@gmail.com>
2020-03-05 17:39:33 -0300

    entidade usuário

---
+++ usuarioprincipal.sql
@@ -0,0 +1,19 @@
+﻿
+	
+	CREATE TABLE public.usuario
+	(
+	  id numeric NOT NULL,
+	  nome character varying,
+	  senha character varying,
+	  CONSTRAINT chave PRIMARY KEY (id)
+	)
+	WITH (
+	  OIDS=FALSE
+	);
+	ALTER TABLE public.usuario
+	  OWNER TO postgres;
+	
+	
+	CREATE SEQUENCE public.sq_usuario;
+	ALTER TABLE public.sq_usuario
+	  OWNER TO postgres;
